968950b57bf3106f5b1c4b98152353e15dad1e43
HuiTaiChen <seeyoulove100@gmail.com>
2019-07-17 10:03:14 +0800

    #0.0.4 ViewModel 串接 DB

--- MVC_Homework/Controllers/MoneyController.cs
+++ MVC_Homework/Controllers/MoneyController.cs
@@ -9,27 +9,41 @@ using System.Data.Entity;
 
 namespace MVC_Homework.Controllers
 {
-    public class MoneyController : Controller
-    {
-        private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
-      // GET: Money
+	public class MoneyController : Controller
+	{
+		private SkillTreeHomeworkEntities db = new SkillTreeHomeworkEntities();
+		// GET: Money
 		public ActionResult Money()
 		{
 			List<AccountViewModel> viewModels = new List<AccountViewModel> { };
-
-			Random rnd = new Random();
-			for (int i = 1; i <= 100; i++)
+			//20190717 改由真DB串接
+			var accountbookList = db.AccountBook.ToList();
+			int i = 0;
+			foreach (var item in accountbookList)
 			{
+
 				AccountViewModel data = new AccountViewModel
 				{
-					No = i,
-					Money = rnd.Next(10000).ToString("###,###"),
-					Date = DateTime.Now.AddDays(-(rnd.Next(100))).ToString("yyyy-MM-dd"),
-					Category = rnd.Next(2) == 1 ? "支出" : "收入"
+					No = ++i,
+					Money = item.Amounttt.ToString("###,###"),
+					Date = item.Dateee.ToString("yyyy-MM-dd"),
+					Category = item.Categoryyy == 0 ? "支出" : "收入"
 				};
 				viewModels.Add(data);
 			}
+			//Random rnd = new Random();
+			//for (int i = 1; i <= 100; i++)
+			//{
+			//	AccountViewModel data = new AccountViewModel
+			//	{
+			//		No = i,
+			//		Money = rnd.Next(10000).ToString("###,###"),
+			//		Date = DateTime.Now.AddDays(-(rnd.Next(100))).ToString("yyyy-MM-dd"),
+			//		Category = rnd.Next(2) == 1 ? "支出" : "收入"
+			//	};				
+			//}
+
 			return View(viewModels);
 		}
-    }
+	}
 }
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 #0.0.1 20190712 Created
 #0.0.2 20190712 Finish first homework
 #0.0.3 20190716 Created DB
+#0.0.4 20190717 ViewModel 串接 DB
